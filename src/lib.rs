c33683da9a3e27c05ac59913365707e5ab967f10
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-08-15 14:34:09 +0859

    fix: clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,6 @@
 //! use treesitter_kind_collector::tree_sitter_kinds;
 //! #[tree_sitter_kinds("asserts/node-types.json")]
 //! struct NodeKindTypes;
-//!
-//! fn main() {
-//!     println!("{}", NodeKindTypes::ARGUMENT);
-//!     println!("{:?}", NodeKindTypes::NODE_TYPES);
-//! }
 //! ```
 //!
 //!
